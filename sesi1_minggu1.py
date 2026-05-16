8d5c5486e766df94b6d4a2525b8477c5da5bff04
Muhammad Aldi Maulana <aldiialdi60@gmail.com>
2026-05-16 21:48:58 +0800

    add: First Learning of Python

---
+++ sesi1_minggu1.py
@@ -0,0 +1,25 @@
+nama = "Muhammad Aldi Maulana"
+umur = 22
+kota = "Makassar"
+
+print(nama)
+print(umur)
+print(kota)
+
+print(nama + " " + str(umur))
+print(len(nama))
+
+print(type(umur))
+
+a = 14
+b = 5
+c = a // b
+
+umur_teks = str(umur)
+
+print(nama + umur_teks)
+
+print(type(umur_teks))
+
+print(c)
+print(type(c))
