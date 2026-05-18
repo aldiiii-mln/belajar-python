410da442e49853c085d76c96b554e8626c78f66f
Muhammad Aldi Maulana <aldiialdi60@gmail.com>
2026-05-18 20:57:21 +0800

    Day 3 Learning of Python

---
+++ sesi3_minggu1.py
@@ -0,0 +1,5 @@
+nama = input("Siapa Namamu? ")
+nilai_ujian = int(input("Berapa Nilai Ujianmu? "))
+
+print(f"Halo {nama}! Nilai Ujianmu {nilai_ujian} ")
+print(f"Nilai tersebut akan menjadi {nilai_ujian * 2} jika digandakan")
