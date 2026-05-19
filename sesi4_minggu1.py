925d38d2bc8610dae2b5e0504ecde7c2e68e08cd
Muhammad Aldi Maulana <aldiialdi60@gmail.com>
2026-05-19 21:23:54 +0800

    Day 4 Learning of Python

--- sesi4_minggu1.py
+++ sesi4_minggu1.py
@@ -2,3 +2,25 @@ nama = input("Siapa Namamu? ")
 umur = int(input("Berapa Umurmu? "))
 tinggi_badan = int(input("Berapa Tinggi Badanmu(cm)? "))
 berat_badan = int(input("Berapa Berat Badanmu(kg)? "))
+
+print("""==== PROFIL KAMU ====
+LIST BERIKUT
+""")
+print(f"Nama        :  {nama}")
+print(f"Umur        :  {umur} tahun")
+print(f"Tinggi      :  {tinggi_badan} cm")
+print(f"Berat       :  {berat_badan} kg")
+
+tinggi_meter = tinggi_badan / 100
+bmi = berat_badan / (tinggi_meter ** 2)
+
+print(f"BMI kamu    :  {bmi:.3f}")
+
+if bmi < 18.5:
+    kategori = "Kurus"
+elif bmi < 25:
+    kategori = "Normal"
+else:
+    kategori = "Gemuk"
+
+print(f"Kategori    :  {kategori}")
