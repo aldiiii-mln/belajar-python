46c6ec3b6b96162a7953e0a653f2bbd8103f4a83
Muhammad Aldi Maulana <aldiialdi60@gmail.com>
2026-05-19 21:22:26 +0800

    Day 4 Learning of Python

---
+++ sesi4_minggu1.py
@@ -0,0 +1,4 @@
+nama = input("Siapa Namamu? ")
+umur = int(input("Berapa Umurmu? "))
+tinggi_badan = int(input("Berapa Tinggi Badanmu(cm)? "))
+berat_badan = int(input("Berapa Berat Badanmu(kg)? "))
